1108c0f861bb7e699870ffc484db984261fcfdd9
Nurullah Erinola <n.erinola@gmx.de>
2018-10-26 22:48:10 +0200

    Add license header

--- src/main/java/de/rub/nds/burp/tlsattacker/gui/UIMain.java
+++ src/main/java/de/rub/nds/burp/tlsattacker/gui/UIMain.java
@@ -1,7 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * TLS-Attacker-BurpExtension
+ * 
+ * Copyright 2018 Ruhr University Bochum / Hackmanit GmbH
+ * 
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0/
  */
 package de.rub.nds.burp.tlsattacker.gui;
 
--- src/main/java/de/rub/nds/burp/tlsattacker/gui/UITab.java
+++ src/main/java/de/rub/nds/burp/tlsattacker/gui/UITab.java
@@ -1,13 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * TLS-Attacker-BurpExtension
+ * 
+ * Copyright 2018 Ruhr University Bochum / Hackmanit GmbH
+ * 
+ * Licensed under Apache License 2.0
+ * http://www.apache.org/licenses/LICENSE-2.0/
  */
 package de.rub.nds.burp.tlsattacker.gui;
 
 import burp.IBurpExtenderCallbacks;
 import burp.ITab;
-import java.awt.Component;
 
 /**
  * An additional tab in Burp Suite.
